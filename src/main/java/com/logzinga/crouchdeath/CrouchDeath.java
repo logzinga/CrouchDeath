5bc800392eb1bfd82956c936de74a7348620230f
logzinga <65839350+logzinga@users.noreply.github.com>
2022-12-30 16:08:51 +1100

    initial commit

---
+++ src/main/java/com/logzinga/crouchdeath/CrouchDeath.java
@@ -0,0 +1,13 @@
+package com.logzinga.crouchdeath;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrouchDeath extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+    
+}
