949173b24c337c2365a6ff808d0604ebb4df5f04
logzinga <65839350+logzinga@users.noreply.github.com>
2022-12-30 17:09:52 +1100

    almost complete

--- src/main/java/com/logzinga/crouchdeath/CrouchDeath.java
+++ src/main/java/com/logzinga/crouchdeath/CrouchDeath.java
@@ -1,13 +1,25 @@
 package com.logzinga.crouchdeath;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CrouchDeath extends JavaPlugin {
+public final class CrouchDeath extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
-    
+    @EventHandler
+        public void onPlayerSneak(PlayerToggleSneakEvent e){
+            Player player = e.getPlayer();
+            player.setHealth(0);
+            player.sendMessage(ChatColor.RED + "dont sneak asshole");
+    }
+
+
 }
